60c561fd1cebc4c7465120a3360935819b2ca705
Peter Danenberg <pcd@roxygen.org>
2012-04-26 03:15:25 -0700

    Docstrings

--- evite_test.py
+++ evite_test.py
@@ -9,12 +9,15 @@ events = None
 queue = None
 
 def init_app():
+    """Initialize the events-list and queue."""
     global events, queue
     queue = Queue()
     events = []
 
 @app.route('/events', methods=['POST'])
 def create_event():
+    """Add an event to the events list so as to assign it an id; put
+    the event in the queue so that the sender picks it up."""
     global events, queue
     event = request.json
     events.append(event)
@@ -24,6 +27,7 @@ def create_event():
 
 @app.route('/events/<int:id>')
 def get_event(id):
+    """Read an event from the events list by id."""
     global events
     return json.dumps(events[id])
 
@@ -37,18 +41,25 @@ class Server:
         self.scheduler = Scheduler(sender)
 
     def send_from_queue(self):
+        """Read blockingly from the event queue, adding events for
+        immediate dispatch to the scheduler.
+
+        NB: If the server is restarted, this thread will block in
+        perpetuity."""
         event = queue.get()
         self.scheduler.add_event(immediate=True, **event)
         self.send_from_queue()
 
     def start(self):
+        """Initialize the app; start the flask server in a process;
+        start the sender-thread that reads from the queue."""
         init_app()
         self.server = Process(target=app.run,
                               kwargs={'port': self.port})
         self.server.start()
 
         # Wait for server to start
-        time.sleep(1)
+        sleep(1)
 
         # Start thread that monitors the queue and sends when we get
         # new events.
@@ -57,6 +68,10 @@ class Server:
         send_from_queue.start()
 
     def stop(self):
+        """Stop the server.
+
+        NB: This does not reap the sender-thread that reads from the
+        queue."""
         self.server.terminate()
 
 class Scheduler:
@@ -86,10 +101,12 @@ class Scheduler:
                     self.messages.append(message)
 
     def start(self):
+        """Start the messages in the queue."""
         for message in self.messages:
             message.start()
 
     def stop(self):
+        """Stop the currently executing messages."""
         for message in self.messages:
             message.cancel()
 
